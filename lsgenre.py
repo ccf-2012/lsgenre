78e5db726fd3dfe0c2003955f3d1c1d8f2940fe4
ccf2012 <crlf66@163.com>
2023-03-17 18:03:45 +0800

    genre_id int

--- lsgenre.py
+++ lsgenre.py
@@ -9,14 +9,14 @@ def listCategory():
         return 
     
     p = TMDbNameParser(ARGS.tmdb_key, 'zh-CN')
-    with open(ARGS.input_file) as in_file:
-        with open(ARGS.output_file, 'a') as out_file:
-            for item in in_file:
-                p.parse(item.rstrip(), useTMDb=True)
-                if p.genre_ids:
-                    if ARGS.genre_id in p.genre_ids:
-                        print("Output: " + item)
-                        out_file.write(item+"\n")
+    with open(ARGS.input_file) as in_file, open(ARGS.output_file, 'a') as out_file:
+        for item in in_file:
+            p.parse(item.rstrip(), useTMDb=True)
+            print(p.genre_ids)
+            if p.genre_ids:
+                if ARGS.genre_id in p.genre_ids:
+                    print("Output: " + item)
+                    out_file.write(item+"\n")
 
 
 def loadArgs():
@@ -24,7 +24,7 @@ def loadArgs():
     parser = argparse.ArgumentParser(description='list media of specified category.')
     parser.add_argument('-i', '--input-file', required=True, help='file of input media list.')
     parser.add_argument('-o', '--output-file', required=True, help='file of output media list.')
-    parser.add_argument('-g', '--genre-id', required=True, help='genre id to be extracted.')
+    parser.add_argument('-g', '--genre-id', type=int, required=True, help='genre id to be extracted.')
     parser.add_argument('--tmdb-key', required=True, help='your TMDb key.')
     ARGS = parser.parse_args()
 
